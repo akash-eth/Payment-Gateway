fc77454be9f217fd1762e953e1556559762b5b58
akashbhardwaj04 <akkiworkspace@gmail.com>
2021-01-25 18:02:12 +0530

    updated

--- basics/app.js
+++
@@ -1,8 +0,0 @@
-// var loggers = require('./logger') 
-const log = require('./logger') //this should always be a Constant to avoid errors at runtime. Not a var
-
-
-//console.log(logger);
-
-log("Indian navy")
-
--- basics/os.js
+++
@@ -1,9 +0,0 @@
-const os = require('os');
-
-var totalMemory = os.totalmem();
-var freeMemory = os.freemem();
-var upTime = os.uptime();
-
-console.log(totalMemory);
-console.log(freeMemory);
-console.log(upTime);
