35e11ae1a3dd30cbec8fcebe4b5f78c48dfd2f7d
akashbhardwaj04 <akkiworkspace@gmail.com>
2021-01-25 09:49:32 +0530

    resolved purchase error

--- payment/server.js
+++ payment/server.js
@@ -1,20 +1,22 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-}
+// if (process.env.NODE_ENV !== 'production') {
+//     require('dotenv').config()
+// }
 
-const stripeSecretKey = process.env.STRIPE_SECRET_KEY
-const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
+// const stripeSecretKey = process.env.STRIPE_SECRET_KEY
+// const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
 
 
-console.log(stripeSecretKey, stripePublicKey)
+// console.log(stripeSecretKey, stripePublicKey)
 
 
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
+
 
 
 app.listen(3000)
