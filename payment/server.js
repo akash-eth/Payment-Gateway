98d92c5656d458c6272960047740e54ccd6fc5bd
akashbhardwaj04 <akkiworkspace@gmail.com>
2021-01-25 15:26:55 +0530

    added token id to server

--- payment/server.js
+++ payment/server.js
@@ -1,12 +1,12 @@
-// if (process.env.NODE_ENV !== 'production') {
-//     require('dotenv').config()
-// }
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
 
-// const stripeSecretKey = process.env.STRIPE_SECRET_KEY
-// const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY
+const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
 
 
-// console.log(stripeSecretKey, stripePublicKey)
+console.log(stripeSecretKey, stripePublicKey)
 
 
 const express = require('express');
@@ -17,6 +17,30 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
+app.get('/store', (req, res) => {
+    fs.readFile('items.json', (err, data) => {
+        if (err) {
+            res.status(500).end();
+        }
+        else{
+            res.render('store.ejs' , {
+                stripePublicKey: stripePublicKey,
+                items: JSON.parse(data)
+            })
+        }
+    })
+})
+
+app.post('/purchase', (req, res) => {
+    fs.readFile('items.json', (err, data) => {
+        if (err) {
+            res.status(500).end()
+        }
+        else{
+            console.log("purchase");
+        }
+    })
+})
 
 
 app.listen(3000)
